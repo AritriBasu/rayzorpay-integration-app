460834c33a20c29ff1d28c8e62aab17e23f5a889
Aritri Basu <basusub@gmail.com>
2021-08-20 04:34:42 +0530

    success and failure routes

--- public/js/index.js
+++ public/js/index.js
@@ -5,7 +5,7 @@ axios.post('/donate').then((info)=>{
 
     var options = {
         key: "rzp_test_ADlY9gCqL0nKJr", // Enter the Key ID generated from the Dashboard
-        amount: "50000", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+        amount: "100", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
         currency: "INR",
         name: "Covid Care",
         "description": "Test Transaction",
